9d85f51c7803524d8630e4f2ea8c7bd744346c0f
Dtn284 <dtn284@gmail.com>
2025-05-01 23:03:56 +0700

    begin

---
+++ user service/dist/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+const connectDB = async () => {
+    try {
+        mongoose.connect(process.env.MONGO_URI, {
+            dbName: "TineFlow",
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+const app = express();
+app.get("/", (req, res) => {
+    res.send("server is working");
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
